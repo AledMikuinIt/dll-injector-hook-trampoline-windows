41b36623a2b8e4311d4fddca21e96b21ce562934
Atom <clement.muzard@etu.umontpellier.fr>
2025-11-28 23:16:11 +0100

    feat: minimalist program that calls MessageBox

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <Windows.h>
+#include <string>
+
+int main() {
+
+    MessageBoxA(
+        NULL,
+        "Initial Message",
+        "Info",
+        MB_OK
+        );
+
+    return 0;
+}
